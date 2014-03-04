7bc4572beef0670d826a0dfc05eaa9292a0e74d5
Jiří Kratochvíl <info@sedrick.cz>
2014-03-04 23:05:06 +0100

    + Add e-mail configuration for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,9 +63,17 @@ Craudio::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  #EMAILER SETTINGS
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => false,
+  }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = {host: "staging.crawdio.sedrick.cz"}
 
 
 end
