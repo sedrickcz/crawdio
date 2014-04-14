c3ec08946b430befaaa06726378ca7aeb6aa5d1a
Jiří Kratochvíl <kratoj@gmail.com>
2014-04-14 09:31:49 +0000

    + Change paypal to production

--- vendor/extensions/orders/app/models/refinery/orders/order.rb
+++ vendor/extensions/orders/app/models/refinery/orders/order.rb
@@ -25,7 +25,7 @@ module Refinery
 
       def paypal_url(return_url, notify_url)
         values = {
-          :business => 'paypal-facilitator@sedrick.cz',
+          :business => 'martin.klima@warhorsestudios.cz',
           :cmd => '_cart',
           :upload => 1,
           :notify_url => notify_url,
@@ -40,7 +40,7 @@ module Refinery
                         "quantity_1" => '1'
         })
 
-        "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+        "https://www.paypal.com/cgi-bin/webscr?" + values.to_query
       end
 
 
