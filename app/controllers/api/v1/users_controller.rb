c6b2cd54e1d97a103b1aaee53a37634dafee77c6
Jiří Kratochvíl <kratoj@gmail.com>
2014-04-23 08:52:59 +0000

    + Fix api for export from forums

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
   def import
-    Refinery::User.create username: params[:user_info][:username], email: params[:user_info][:email], activated: true, age: 18, password: SecureRandom.hex(16), hash_pasword: params[:user_info][:pasword], salt: params[:user_info][:salt]
+    Refinery::User.create! username: params[:user_info][:username], email: params[:user_info][:email], activated: true, age: 18, password: SecureRandom.hex(16), hash_password: params[:user_info][:pasword], salt: params[:user_info][:salt], name: "Fill your name", street: "Fill your street", city: "Fill your city", country: "US"
     render json: {}, status: 200
   end
 end
