69ce745ca36f4e327fe0281ed199fced7c59864c
Jiří Kratochvíl <kratoj@gmail.com>
2014-06-09 13:23:09 +0000

    + If user has physical pledge than no shipping price for upgrades

--- vendor/extensions/orders/app/models/refinery/orders/order.rb
+++ vendor/extensions/orders/app/models/refinery/orders/order.rb
@@ -24,7 +24,11 @@ module Refinery
         if is_physical?
           zero_shipping_price = ['US', 'CA', 'CZ', 'CH', 'NO', 'AT', 'BE', 'BG', 'HR', 'CY', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB']
           unless zero_shipping_price.include?(country)
-            total_price = price + tier.project.shipping_price
+            if user.highest_tier.physical
+              total_price = price
+            else
+              total_price = price + tier.project.shipping_price
+            end
           end
         end
         total_price
